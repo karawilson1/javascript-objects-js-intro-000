3a98149453ae3eada586af2ea3d5b455efb27e9e
Wilson <karaewilson16@gmail.com>
2021-01-15 02:43:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,6 +5,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete (playlist.PhilOchs)
+  delete (playlist.SlowDive)
   return playlist
 }
