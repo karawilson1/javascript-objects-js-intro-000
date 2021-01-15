ec9c4a06d29181101a156e167d4b5f1ed9067816
Wilson <karaewilson16@gmail.com>
2021-01-15 02:27:22 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { Phil Ochs: "SlowDive" };
+var playlist = { PhilOchs: "SlowDive" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { PhilOchs: 'Slowdive'})
