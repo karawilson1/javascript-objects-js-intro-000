67b64a64dc463f1caa439e8886c31ff3767604a6
Wilson <karaewilson16@gmail.com>
2021-01-15 02:33:56 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = { 'Phil Ochs': 'SlowDive' };
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { 'Phil Ochs': ['myBloodyValentine']})
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.'SlowDive'
+}
