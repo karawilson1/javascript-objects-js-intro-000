9c78c9eae22242ddad4c594e688b8736f8cd9a97
Wilson <karaewilson16@gmail.com>
2021-01-15 02:21:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,5 @@
 var playlist = { DuaLipa: "Levitating" };
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({ playlist, { [ArianaGrande]: "Just Like Magic", }, [TaylorSwift]: "Mad Woman" })
+}
