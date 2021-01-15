a37cf242aa7b01a60188da96989513943b05fd2a
Wilson <karaewilson16@gmail.com>
2021-01-15 02:35:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,5 +5,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.songTitle
+  delete playlist.myBloodyValentine
 }
